11731a3576c4ca2155533a3b0f1d84065dcc21cb
Akeem Taiwo (Work) <akeem.taiwo@dataireland.ie>
2017-03-15 15:53:01 +0000

    Linked List

---
+++ DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoublyLinkedList
+{
+    public class LinkedList<T> : ICollection<T>
+    {
+        public LinkedListNode<T> Head { get; private set; }
+        public LinkedListNode<T> Tail { get; private set; }
+
+        public int Count { get; set; }
+
+        public bool IsReadOnly => false;
+
+        public void AddFirst(T value)
+        {
+            AddFirst(new LinkedListNode<T>(value));
+        }
+
+        public void AddFirst(LinkedListNode<T> node)
+        {
+            LinkedListNode<T> temp = Head;
+
+            Head = node;
+
+            Head.Next = temp;
+            
+            if (Count == 0)
+            {
+                Tail = Head;
+            }
+            else
+            {
+                temp.Previous = Head;
+            }
+
+            Count++;
+        }
+
+        public void AddLast(T value)
+        {
+            AddLast(new LinkedListNode<T>(value));
+        }
+
+        public void AddLast(LinkedListNode<T> node)
+        {
+            if (node.Next != null)
+            {
+                if (Count == 0)
+                {
+                    Head = node;
+                }
+                else
+                {
+                    Tail.Next = node;
+
+                    node.Previous = Tail;
+                }
+
+                Tail = node;
+
+                Count++;
+            }
+
+            throw new Exception("Node is not a new node.");
+        }
+
+        public void RemoveFirst()
+        {
+
+            if (Count != 0)
+            {
+                Head = Head.Next;
+
+                Count--;
+
+                if (Count == 0)
+                {
+                    Tail = null;
+                }
+                else
+                {
+                    Head.Previous = null;
+                }
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if (Count != 0)
+            {
+                if (Count == 1)
+                {
+                    Tail = null;
+                    Head = null;
+
+                    Count--;
+                }
+                else
+                {
+                    // Before: Head --> 3 --> 5 --> 7
+                    //         Tail = 7
+                    // After:  Head --> 3 --> 5 --> null
+                    //         Tail = 5
+                    // Null out 5's Next pointer
+                    Tail.Previous.Next = null;
+                    Tail = Tail.Previous;
+                }
+
+                Count--;
+            }
+
+        }
+
+        public void Add(T item)
+        {
+            AddFirst(item);
+        }
+
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            LinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            LinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                array[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator<T> System.Collections.Generic.IEnumerable<T>.GetEnumerator()
+        {
+            LinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return ((System.Collections.Generic.IEnumerable<T>)this).GetEnumerator();
+        }
+
+
+        public bool Remove(T item)
+        {
+            LinkedListNode<T> previous = null;
+            LinkedListNode<T> current = Head;
+
+            // 1: Empty list - do nothing
+            // 2: Single node: (previous is null)
+            // 3: Many nodes
+            //    a: node to remove is the first node
+            //    b: node to remove is the middle or last
+
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (current.Next == null)
+                        {
+                            Tail = previous;
+                        }
+                        else
+                        {
+                            // Before: Head -> 3 <-> 5 <-> 7 -> null
+                            // After:  Head -> 3 <-------> 7 -> null
+
+                            // previous = 3
+                            // current = 5
+                            // current.Next = 7
+                            // So... 7.Previous = 3
+                            current.Next.Previous = previous;
+                        }
+
+                        Count--;
+                    }
+                    else
+                    {
+                        RemoveFirst();
+                    }
+
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class LinkedList<T> : ICollection<T>
+    {
+        public LinkedListNode<T> Head { get; private set; }
+        public LinkedListNode<T> Tail { get; private set; }
+
+        public int Count { get; set; }
+
+        public bool IsReadOnly => false;
+
+        public void AddFirst(T value)
+        {
+            AddFirst(new LinkedListNode<T>(value));
+        }
+
+        public void AddFirst(LinkedListNode<T> node)
+        {
+            LinkedListNode<T> temp = Head;
+
+            Head = node;
+
+            Head.Next = temp;
+
+            Count++;
+
+            if (Count == 1)
+            {
+                Tail = Head;
+            }
+        }
+
+        public void AddLast(T value)
+        {
+            AddLast(new LinkedListNode<T>(value));
+        }
+
+        public void AddLast(LinkedListNode<T> node)
+        {
+            if (node.Next != null)
+            {
+                if (Count == 0)
+                {
+                    Head = node;
+                }
+                else
+                {
+                    Tail.Next = node;
+                }
+
+                Tail = node;
+
+                Count++;
+            }
+
+            throw new Exception("Node is not a new node.");
+        }
+
+        public void RemoveFirst()
+        {
+
+            if (Count != 0)
+            {
+                Head = Head.Next;
+
+                Count--;
+
+                if (Count == 0)
+                {
+                    Tail = null;
+                }
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if (Count != 0)
+            {
+                if (Count == 1)
+                {
+                    Tail = null;
+                    Head = null;
+
+                    Count--;
+                }
+                else
+                {
+                    LinkedListNode<T> current = Head;
+                    while (current.Next != Tail)
+                    {
+                        current = current.Next;
+                    }
+
+                    current.Next = null;
+                    Tail = current;
+                }
+
+                Count--;
+            }
+
+        }
+
+        public void Add(T item)
+        {
+            AddFirst(item);
+        }
+
+
+
+        public void Clear()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            LinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            LinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                array[arrayIndex++] = current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator<T> System.Collections.Generic.IEnumerable<T>.GetEnumerator()
+        {
+            LinkedListNode<T> current = Head;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return ((System.Collections.Generic.IEnumerable<T>)this).GetEnumerator();
+        }
+
+
+        public bool Remove(T item)
+        {
+            LinkedListNode<T> previous = null;
+            LinkedListNode<T> current = Head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(item))
+                {
+                    if (previous != null)
+                    {
+                        previous.Next = current.Next;
+
+                        if (current.Next == null)
+                        {
+                            Tail = previous;
+                        }
+
+                        Count--;
+                    }
+                    else
+                    {
+                        RemoveFirst();
+                    }
+
+                    return true;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+
+            return false;
+        }
+
+    }
+}
